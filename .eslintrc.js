57c328e3e00f9ed0f5a611261adbf049f4a9f937
jeremN <jeremne@gmail.com>
2021-03-30 23:25:50 +0200

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,20 @@
 module.exports = {
     "parser": "@typescript-eslint/parser",
     "plugins": ["@typescript-eslint"],
-    "extends": ["airbnb-typescript/base"],
+    "extends": [
+        "airbnb-typescript/base",
+        "eslint-config-prettier",
+        "eslint-config-prettier/@typescript-eslint"
+    ],
     "parserOptions": {
-        "project": "./tsconfig.json"
+        "project": "./tsconfig.json",
+        "ecmaVersion": 2019,
+        "sourceType": "module"
     },
-    "ignorePatterns": ["webpack.dev.js", "webpack.prod.js", "./dist", ".eslintrc.js"]
+    "ignorePatterns": [
+        "webpack.dev.js", 
+        "webpack.prod.js", 
+        "./dist", 
+        ".eslintrc.js"
+    ]
 };
