54622f98a63232deaf9a4ff98f09bd9763be5173
jeremN <jeremne@gmail.com>
2021-04-17 17:30:25 +0200

    update packages and adding prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "plugins": ["@typescript-eslint"],
-    "extends": [
-        "airbnb-typescript/base",
-        "eslint-config-prettier",
-        "eslint-config-prettier/@typescript-eslint"
-    ],
-    "parserOptions": {
-        "project": "./tsconfig.json",
-        "ecmaVersion": 2019,
-        "sourceType": "module"
-    },
-    "ignorePatterns": [
-        "webpack.dev.js", 
-        "webpack.prod.js", 
-        "./dist", 
-        ".eslintrc.js"
-    ]
-};
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'airbnb-typescript/base',
+    'eslint-config-prettier/@typescript-eslint',
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    'webpack.dev.js',
+    'webpack.prod.js',
+    './dist',
+    '.eslintrc.js',
+  ],
+  rules: {
+    'prettier/prettier': 2,
+  },
+}
